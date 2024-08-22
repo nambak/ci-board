c5c910a06f8719551742528e9ace670ba0dff813
남성룡 <nambak80@gmail.com>
2024-08-22 17:54:09 +0900

    debugbar 설치

--- application/views/board/list_v.php
+++ application/views/board/list_v.php
@@ -2,13 +2,13 @@
     <h1>게시판</h1>
     <table class="table table-striped">
         <thead class="table-dark">
-            <tr>
-                <th scope="col">번호</th>
-                <th scope="col">제목</th>
-                <th scope="col">작성자</th>
-                <th scope="col">조회수</th>
-                <th scope="col">등록일</th>
-            </tr>
+        <tr>
+            <th scope="col">번호</th>
+            <th scope="col">제목</th>
+            <th scope="col">작성자</th>
+            <th scope="col">조회수</th>
+            <th scope="col">등록일</th>
+        </tr>
         </thead>
         <tbody>
 
@@ -21,14 +21,23 @@
     </nav>
 </article>
 <script defer>
-    $(document).ready(function() {
-        //$.ajax({
-        //  url: '/rest/board',
-        //  type: 'GET',
-        //  data: {
-        //    id: <?php //= $this->input->get('id', TRUE); ?>//,
-        //    page: 1,
-        //  }
-        //})
-    })
+  $(document).ready(function () {
+    $.ajax({
+      url: '/rest/board/<?= $this->input->get('id', TRUE) ?: 1; ?>',
+      type: 'GET',
+      data: {
+        page: 1,
+      },
+      error: function (error) {
+        Swal.fire({
+          title: 'Error ' + error.status ,
+          text: error.statusText,
+          icon: 'error',
+        });
+      },
+      success: function (response) {
+        console.log(response);
+      }
+    });
+  })
 </script>
