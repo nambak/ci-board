5645085f822c2431eca176c52d62705fb9526f57
남성룡 <nambak80@gmail.com>
2024-12-18 19:27:35 +0900

    - 게시물 수정 API 완료 - 게시물 수정 성공시 게시물 상세 페이지로 이동 추가

--- application/controllers/rest/Post.php
+++ application/controllers/rest/Post.php
@@ -8,11 +8,11 @@ class Post extends RestController
     public function __construct()
     {
         parent::__construct();
+        $this->load->model('post_m');
     }
 
     public function detail_get()
     {
-        $this->load->model('post_m');
         $id = $this->get('id', true);
         $post = $this->post_m->get($id);
 
@@ -21,5 +21,21 @@ class Post extends RestController
         }
 
         $this->response($post, 200);
+    }
+
+    public function update_post()
+    {
+        $id = $this->input->get('id', true);
+        $title = $this->input->post('title', true);
+        $content = $this->input->post('content', true);
+
+        $result = $this->post_m->update($id, $title, $content);
+
+        if ($result) {
+            $this->response('success', 200);
+        } else {
+            $this->response('database failed: ' . $result , 500);
+        }
+
     }
 }
--- application/views/post/edit_v.php
+++ application/views/post/edit_v.php
@@ -1,41 +1,121 @@
-<article class="container mt-5" id="post_detail">
+<article class="container mt-5" id="post_edit">
     <!-- 게시글 제목 -->
-    <div class="row mb-4">
+    <div class="row mb-2">
         <div class="col">
-            <h2 class="border-bottom pb-3" id="title"></h2>
-        </div>
-    </div>
-    <!-- 게시글 정보 -->
-    <div class="row mb-4">
-        <div class="col">
-            <div class="d-flex justify-content-between align-items-center">
-                <div>
-                    <span class="me-3">작성자: <span id="writer"></span></span>
-                    <span class="me-3">작성일: <span id="createdAt"></span></span>
-                    <span>조회수: <span id="views"></span></span>
-                </div>
-            </div>
+            <h2 class="border-bottom pb-3">
+                <input type="text" class="form-control" id="title" name="title">
+            </h2>
         </div>
     </div>
     <!-- 게시글 내용 -->
     <div class="row mb-4">
         <div class="col">
             <div class="card">
-                <div class="card-body min-vh-50" id="content"></div>
+                <textarea class="form-control" id="content" rows="10" name="content"></textarea>
             </div>
         </div>
     </div>
     <!-- 버튼 영역 -->
     <div class="row mb-5">
         <div class="col">
-            <div class="d-flex justify-content-between">
+            <div class="d-flex justify-content-end">
                 <div>
-                    <button id="redirectBoardListButton" class="btn btn-outline-secondary me-2">목록으로</button>
+                    <button id="cancelButton" class="btn btn-outline-secondary me-2">취소</button>
+                    <button id="confirmEdit" class="btn btn-primary me-2">수정</button>
                 </div>
             </div>
         </div>
     </div>
 </article>
 <script defer>
+    let pageId = '#post_edit ';
 
+    function initCancelButton(postId) {
+        $(pageId + '#cancelButton').on('click', () => {
+            location.href = `/post/detail?id=${postId}`;
+        });
+    }
+
+    function init(postId) {
+        $.ajax({
+            url: '/rest/post/detail',
+            type: 'GET',
+            dataType: 'json',
+            data: {
+                id: postId
+            },
+            success: (data) => {
+                if (data) {
+                    $(pageId + 'input[name=title]').val(data.title);
+                    $(pageId + 'textarea[name=content]').val(data.content);
+                }
+            },
+            error: (error) => {
+                Swal.fire({
+                    title: `${error.status} ${error.statusText}`,
+                    icon: 'error'
+                });
+            }
+        });
+    }
+
+    function initConfirmButton(postId) {
+        $(pageId + '#confirmEdit').on('click', () => updatePost(postId));
+    }
+
+    function updatePost(postId) {
+        const title = $(pageId + 'input[name=title]').val();
+        const content = $(pageId + 'textarea[name=content]').val();
+
+        if (!title) {
+            Swal.fire({
+                icon: 'warning',
+                text: '제목을 입력해 주세요.'
+            });
+
+            return false;
+        }
+
+        if (!content) {
+            Swal.fire({
+                icon: 'warning',
+                text: '내용을 입력해 주세요.'
+            });
+
+            return false;
+        }
+
+        $.ajax({
+            url: '/rest/post/update?id=' + postId,
+            type: 'POST',
+            data: {
+                title: title,
+                content: content
+            },
+            success: (response) => {
+                if (response === 'success') {
+                    Swal.fire({
+                        title: '수정되었습니다.',
+                        icon: 'success'
+                    }).then((result) => {
+                        if (result.isConfirmed) {
+                            location.href = `/post/detail?id=${postId}`;
+                        }
+                    });
+                }
+            },
+            error: (error) => {
+                Swal.fire({
+                    title: `${error.status} ${error.statusText}`,
+                    icon: 'error'
+                });
+            }
+        });
+    }
+
+    $(document).ready(() => {
+        initCancelButton(<?= $id ?>);
+        initConfirmButton(<?= $id ?>);
+        init(<?= $id ?>);
+    });
 </script>
