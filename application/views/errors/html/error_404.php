cb1899637beee82deba9e8c80fe3a281a27adf89
Alex Nam <nambak80@gmail.com>
2025-06-29 18:59:25 +0900

    404 에러 페이지 헤더 적용 삭제

--- application/views/errors/html/error_404.php
+++ application/views/errors/html/error_404.php
@@ -30,28 +30,6 @@ defined('BASEPATH') || exit('No direct script access allowed');
     </style>
 </head>
 <body>
-<header class="p-3 text-bg-dark">
-    <div class="container">
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <div class="bg-white text-black p-2 rounded-3 me-3">CI3Board</div>
-            <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="/board?id=1" class="nav-link px-2 text-white">게시판</a></li>
-            </ul>
-            <div class="text-end">
-                <?php if($this->session->userdata('logged_in')): ?>
-                    <span class="text-white me-3">
-                            <?= $this->session->userdata('user_name') ?>님 환영합니다
-                        </span>
-                    <a href="/logout" class="btn btn-outline-light">로그아웃</a>
-                <?php else: ?>
-                    <a href="/login" class="btn btn-outline-light me-2">로그인</a>
-                    <a href="/register" class="btn btn-warning">가입</a>
-                <?php endif; ?>
-            </div>
-        </div>
-    </div>
-</header>
-
 <div id="container">
     <div class="row w-100 justify-content-center">
         <div class="col-md-8 text-center">
