aa3458990fc25f79049be6ed15ed76e401b43324
Alex Nam <nambak80@gmail.com>
2025-07-15 16:40:05 +0900

    데이터베이스 연결 설정 수정

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,21 +1,27 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+use DebugBar\StandardDebugBar;
+
 class MY_Controller extends CI_Controller {
+    protected $debugbar;
+    protected $debugbarRenderer;
 
     public function __construct() {
         parent::__construct();
+        $this->debugbar = new StandardDebugBar();
+        $this->debugbarRenderer = $this->debugbar->getJavascriptRenderer();
     }
 
     public function _output($output) {
-        // header view
+        // header view에 debugbarRenderer 전달
         $header = $this->load->view('header_v', [
-            'debugbarRenderer' => null
+            'debugbarRenderer' => $this->debugbarRenderer
         ], TRUE);
 
-        // footer view
+        // footer view에 debugbarRenderer 전달
         $footer = $this->load->view('footer_v', [
-            'debugbarRenderer' => null
+            'debugbarRenderer' => $this->debugbarRenderer
         ], TRUE);
 
         echo $header . $output . $footer;
